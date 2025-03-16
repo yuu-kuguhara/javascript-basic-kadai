a6149070c61160f293ae8112858eeedd00cb3a02
yuu-kuguhara <you20010819@gmail.com>
2025-03-16 23:43:15 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date('2025-3-16');
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+
+console.log(year + '年' + month + '月' + date + '日');
